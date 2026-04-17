2be0f4ac025b1f6bc397572bd129da09b2881b49
Jacob Anderson <jacobdanderson@gmail.com>
2026-04-17 11:16:10 -0400

    Add missing AI Level 2 project packs

---
+++ ML8-Master-Project/main.py
@@ -0,0 +1,3 @@
+"""
+For the master project, you’ll be selecting a dataset of your choice and completing a machine learning related project. This project can be any of the topics we discussed so far and can also use any of the algorithms you have learned.
+"""
